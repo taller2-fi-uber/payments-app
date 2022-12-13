51ec30dbadaac7490caad68f0d45242a346d0ad1
Daniel Pons <daniel@siltron.com.ar>
2022-12-13 15:36:47 -0300

    Agrega transfer

--- src/services/services.js
+++ src/services/services.js
@@ -1,7 +1,9 @@
 const walletService = require("./wallets");
 const contractInteraction = require("./contractInteraction");
+const transfer = require("./transfer");
 
 module.exports = ({ config }) => ({
   walletService: walletService({ config }),
   contractInteraction: contractInteraction({ config }),
+  transferService: transfer({ config })
 });
